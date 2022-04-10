2ba13d1de084430de2ef021ab00e59fa15f354b4
deniz karakas <denizkarakas@yaani.com>
2022-04-11 02:11:53 +0300

    middlewares

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const mongoose = require("mongoose");
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const fileUpload = require('express-fileupload');
 const pageRoute = require("./routes/pageRoute");
 const courseRoute = require("./routes/courseRoute");
 const categoryRoute = require("./routes/categoryRoute");
+const userRoute = require("./routes/userRoute");
+
 
 const app = express();
 
@@ -20,18 +24,33 @@ mongoose.connect('mongodb://localhost/smartedu-db',{
 //Template Engine
 app.set('view engine', 'ejs');
 
+//Global variables
+global.userIN = null;
+
   
 //Middlewares
 app.use(express.static('public'));
 app.use(fileUpload());
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(session({
+  secret: 'my_keyboard_cat',
+  resave: false,
+  saveUninitialized: true,
+  store: MongoStore.create({ mongoUrl: 'mongodb://localhost/smartedu-db' }),
+}))
 
 
 //Routes
+app.use('*', (req, res, next) => {
+  userIN = req.session.userID;
+  next();
+});
 app.use('/', pageRoute);
 app.use('/courses', courseRoute);
 app.use('/categories', categoryRoute);
+app.use('/users', userRoute);
+
 
   
 const port = 3000;
